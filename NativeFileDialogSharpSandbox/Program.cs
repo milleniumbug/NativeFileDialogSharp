bb07ec43cd45f5063866f35080c833fdb25b3346
milleniumbug <milleniumbug@protonmail.com>
2021-12-26 21:04:24 +0100

    First release

---
+++ NativeFileDialogSharpSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NativeFileDialogSharp.Native;
+
+var result = Dialog.FileOpenMultiple();
+
+Console.WriteLine($"Path: {result.Path}, IsError {result.IsError}, IsOk {result.IsOk}, IsCancelled {result.IsCancelled}, ErrorMessage {result.ErrorMessage}");
+if (result.Paths != null)
+{
+    Console.WriteLine("Paths");
+    Console.WriteLine(string.Join("\n", result.Paths));
+}
